493f03b43f36a6f63088a3c2a5942399140127e4
hargunmarya <hargunmarya2726@gmail.com>
2019-06-10 19:07:10 -0400

    [Hargun] classes and display

--- Day3ClassAndStructure/main.swift
+++ Day3ClassAndStructure/main.swift
@@ -9,4 +9,36 @@
 import Foundation
 
 print("Hello, World!")
+class Student
+{
+    var studentId:Int = 0
+    var firstName:String?
+    var lastName:String!
+    var result:String!
+    var gender:Bool = false
+    //var result:String = ""
+    func setValues()
+    {
+        
+        self .studentId=0
+        self.firstName="hargun"
+        self.lastName="marya"
+        
+    }
+    
+    
+    func display()
+    {
+        print("Student ID:\(self.studentId)")
+         print("Student First Name:\(self.firstName)")
+         print("Student Last Name:\(self.lastName)")
+         print("Student Gender:\(self.gender)")
+         print("Student Result:\(self.result)")
+    }
+    
+}
+var s1=Student()
+s1.display()
+var s2 = Student()
+s2=Student()
 

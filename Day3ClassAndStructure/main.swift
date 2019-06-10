a08da0c23ef3c3788d25a0b2c98181b2c502eab6
hargunmarya <hargunmarya2726@gmail.com>
2019-06-10 18:33:25 -0400

    Initial Commit

---
+++ Day3ClassAndStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day3ClassAndStructure
+//
+//  Created by Hargun Marya on 2019-06-10.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
